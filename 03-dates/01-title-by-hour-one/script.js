1d43749003ec28f9299faf19115c23d193f04001
unknown <antoniopaulobaciu@gmail.com>
2021-04-22 17:46:48 +0200

    Finished dates ex1 & ex2

--- 03-dates/01-title-by-hour-one/script.js
+++ 03-dates/01-title-by-hour-one/script.js
@@ -9,10 +9,7 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-
-    // your code here
-
+(function () {
+  document.getElementById("target").textContent =
+    new Date().getHours() < 18 ? "Hello" : "Good Evening";
 })();
--- 03-dates/02-title-by-hour-two/script.js
+++ 03-dates/02-title-by-hour-two/script.js
@@ -9,10 +9,25 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-
-    // your code here
-
+//  Ternary if - else if - else
+(function () {
+  document.getElementById("target").textContent =
+    new Date().getHours() < 17
+      ? "Hello"
+      : new Date().getHours() >= 17 && new Date().getMinutes() < 30
+      ? "Hello"
+      : "Good Evening ";
 })();
+
+// Reusable function
+// (function () {
+//   document.getElementById("target").textContent = greeting(18, 45);
+// })();
+
+// const greeting = (hours, minutes) => {
+//   new Date().getHours() < hours
+//     ? "Hello"
+//     : new Date().getHours() >= hours && new Date().getMinutes() < minutes
+//     ? "Hello"
+//     : "Good Evening ";
+// };
