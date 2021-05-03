13a0a2a262e107e630e6f03c400bf20eaf6fa7ec
unknown <antoniopaulobaciu@gmail.com>
2021-05-03 16:57:07 +0200

    Finished Dom - 07

--- 06-dom/07-generate-table-two-Optional/script.js
+++ 06-dom/07-generate-table-two-Optional/script.js
@@ -9,8 +9,18 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  const tableTag = document.createElement("table"); // Creates 'table tag'
+  const tableRow = document.createElement("tr"); // Creates 'table row'
+  const table = target.appendChild(tableTag); // Another way to place 'table tag' inside the' target div'
 
-    // your code here
+  for (rows = 0; rows < 10; rows++) {
+    let newRow = table.insertRow();
 
+    for (cell = 0; cell < 10; cell++) {
+      let newCell = newRow.insertCell();
+      let innerCell = (rows + 1) * (cell + 1);
+      newCell.appendChild(document.createTextNode(innerCell));
+    }
+  }
 })();
