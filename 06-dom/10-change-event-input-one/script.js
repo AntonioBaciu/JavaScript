31e7c4cd39727cbb12637e908ac5fc15e98502c9
unknown <antoniopaulobaciu@gmail.com>
2021-05-04 19:06:47 +0200

    Updated dom - 06

--- 06-dom/10-change-event-input-one/script.js
+++ 06-dom/10-change-event-input-one/script.js
@@ -10,32 +10,14 @@
 // You will have time to focus on it later.
 
 (function () {
-  // Sets character limit
-  const inputLimit = document
-    .getElementById("pass-one")
-    .setAttribute("maxlength", "10");
+  let inputValue = document.getElementById("pass-one");
+  document.getElementById("pass-one").setAttribute("maxlength", "10");
 
-  // Assigns input and counter to variables
-  let passValue = document.getElementById("pass-one");
-  let valCount = document.getElementById("counter");
+  let counterValue = document.getElementById("counter");
 
-  // Adds event listener for the input
-  // When any key is pressed to provide input,
-  // the counter is increased using the for loop.
-  passValue.addEventListener("keydown", function () {
-    valCount = passValue.value.length + 1;
-    for (i = 0; i < 10; i++) {
-      if (valCount < 11) {
-        document.querySelector(".indicator").textContent = `${valCount}/10`;
-        if (passValue.length < valCount) {
-          document.querySelector(".indicator").textContent = `${valCount--}/10`;
-        }
-      }
-    }
-  });
+  inputValue.addEventListener("input", counterStatus);
+
+  function counterStatus() {
+    counterValue.innerHTML = inputValue.value.length + "/10";
+  }
 })();
-
-// Kinda working.
-// To fix:
-// *decreasing counter when the input length decreases
-// *counter must get to 0 when input is empty (stays at 1)
