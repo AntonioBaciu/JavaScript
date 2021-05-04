bcdbd3d98db89b1741863cd82a1d58661cf78729
unknown <antoniopaulobaciu@gmail.com>
2021-05-04 10:48:57 +0200

    Finished Fetch - 01

--- 08-fetch/01-list-to-console/script.js
+++ 08-fetch/01-list-to-console/script.js
@@ -10,5 +10,41 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  document.getElementById("run").addEventListener("click", () => {
+    fetch("../../_shared/api.json")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  });
 })();
+
+/* 
+#1 Get the file:
+
+fetch("../../_shared/api.json") -> via [fetch] we can retrieve the json file
+                                -> [fetch] return a promise 
+"example:" 
+get-the-file-and-it's-content(file-path)
+
+#2 Return and convert the text file:
+
+[fetch("path")] -> returns a promise
+
+We can handle the promise with a [.then] method.
+.then(function(response) -> will use the [.then] method to handle the returned promise
+ function(response) -> will use a function with the [response] parameter to take the received response and return the response 
+                    -> the return will be a text 
+{
+  return response.json() -> Using the [.json()] method we can convert the returned text into a [.json] file 
+})
+
+#3 Use the file
+
+.then(function(data) -> using a new [.then] method, we will handle the data from the [.json] file
+
+Time to console log the data form the [.json] file with console.log(data);
+
+*/
